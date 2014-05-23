34c1a969089b57faac4ffa5472ef7c65f322e62c
Dave Hanlon <daveh@vertigo.com>
2014-05-22 21:17:20 -0500

    Creating the simple Workout model

---
+++ WhatsMyRun/DataModel/Workouts/WorkoutDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WhatsMyRun.DataModel.Workouts
+{
+    public class WorkoutDataModel
+    {
+        public DateTime StartTime { get; set; }
+        public string Notes { get; set; }
+        public TimeSpan ElapsedTimeInSeconds { get; set; }
+        public double ActiveTimeInSeconds { get; set; }
+        public double DistanceInMeters { get; set; }
+        public int TotalSteps { get; set; }
+        public double AverageSpeedInX { get; set; }
+        public double MetabolicEnergeyTotal { get; set; }
+    }
+}
