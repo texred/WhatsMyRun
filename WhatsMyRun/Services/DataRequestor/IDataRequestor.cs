0ef3eb428f21bf8bf4e81c0b71289aa1658841cd
Dave Hanlon <daveh@vertigo.com>
2014-05-22 21:05:48 -0500

    Adding in sample data support and data requestors

---
+++ WhatsMyRun/Services/DataRequestor/IDataRequestor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WhatsMyRun.Services.DataRequestor
+{
+    public interface IDataRequestor
+    {
+        Task<T> GetDataAsync<T>(Uri uri);
+
+        Task<T> PostDataAsync<T>(Uri uri, IDictionary<string, string> parameters);
+    }
+}
---
+++ WhatsMyRun/Services/DataRequestor/SampleDataRequestor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WhatsMyRun.Services.DataRequestor
+{
+    public class SampleDataRequestor : IDataRequestor
+    {
+        public async Task<T> GetDataAsync<T>(Uri uri)
+        {
+            if (uri.AbsolutePath.Contains("workout"))
+            {
+                uri = new Uri("https://dl.dropboxusercontent.com/u/27461593/WhatsMyRunSampleData/Workouts.json");
+            }
+
+            using (var client = new HttpClient())
+            using (var response = await client.GetAsync(uri))
+            {
+                if (response == null) return default(T);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                // Deserialize
+            }
+        }
+
+        public Task<T> PostDataAsync<T>(Uri uri, IDictionary<string, string> parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
