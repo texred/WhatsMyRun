a19217cc3bdefda70c7bc9628fab96eabe2f6d97
Ryan Betker <ryan_betker@hotmail.com>
2014-05-22 20:21:13 -0500

    Adding data fetcher class - needs OAuth still

--- WhatsMyRun/Services/MapMyRun/MapMyFitnessDataFetcher.cs
+++ WhatsMyRun/Services/MapMyRun/MapMyFitnessDataFetcher.cs
@@ -12,6 +12,12 @@ namespace WhatsMyRun.Services.MapMyRun
         public Responses.WorkoutResponse.WorkoutResponse GetWorkoutData(int workoutID)
         {
             //TODO Also needs to take into account OAuth to get the data. With this being a "DAL", that may muck up the design having the popup to the user to get their approval for auth
+            //const string clientID = "ypn4h2h2jrjpkj5zwm28e2d97qewkaed";
+            //const string redirectUri = "http://localhost.mapmyapi.com:12345/callback";
+            //var authorizeUrl = String.Format("https://www.mapmyfitness.com/v7.0/oauth2/authorize/?client_id={0}&response_type=code&redirect_uri={1}", clientID, redirectUri);
+            //https://oauth2-api.mapmyapi.com/v7.0/oauth2/access_token/
+            workoutID = 572927641;
+            //Actual method call done to: String.Format("https://oauth2-api.mapmyapi.com/v7.0/workout/{0}", workoutID);
 
             //Alternatively, in a testing project, use Moq to change this type of data and just pass in the IGetWorkoutData to your business layer
             return new Responses.WorkoutResponse.WorkoutResponse()
