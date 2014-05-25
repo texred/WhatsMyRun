5f2938bd93da74b233e296138bfd00dc857fae2e
Ryan Betker <ryan_betker@hotmail.com>
2014-05-24 23:08:34 -0500

    Adding in missing WorkoutDataProvider

---
+++ WhatsMyRun.Services/DataProviders/Workouts/IWorkoutDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhatsMyRun.DataModel.Workouts;
+
+namespace WhatsMyRun.Services.DataProviders.Workouts
+{
+    public interface IWorkoutDataProvider
+    {
+        Task<IEnumerable<WorkoutDataModel>> GetWorkoutsForUserAsync(int userId);
+    }
+}
