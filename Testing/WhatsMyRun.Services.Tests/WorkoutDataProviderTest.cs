144b7e02a89deb1406c4f24e35dd6d2b25851501
Ryan Betker <ryan_betker@hotmail.com>
2014-05-24 22:30:34 -0500

    Splitting out projects into 3 separate pieces for testing. Also adding testing project with Moq. Missing WorkoutDataProvider though

---
+++ Testing/WhatsMyRun.Services.Tests/WorkoutDataProviderTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace WhatsMyRun.Services.Tests
+{
+    [TestClass]
+    public class WorkoutDataProviderTest
+    {
+        [TestMethod]
+        public void GetWorkoutsForUserAsync_Default_CallsDataProvider()
+        {
+            Assert.Fail("unwritten");
+            //Moq 4.2 by default doesn't work with win store apps
+            //will see about making the other projects be PCLs, but that means that any win classes can't go in the service.
+        }
+    }
+}
---
+++ Testing/WhatsMyRun.Tests/WorkoutDataProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhatsMyRun.Services.DataRequestor;
+using Moq;
+
+namespace WhatsMyRun.Tests
+{
+    [TestClass]
+    public class WorkoutDataProviderTest
+    {
+        [TestMethod]
+        public void WorkoutDataProvider_Default_CallsRequestor()
+        {
+            var mockRequestor = new Moq.Mock<IDataRequestor>();
+            //new WorkoutDataProvider(mockRequestor.Object);
+            //mockRequestor.Verify(r => r.GetDataAsync(It.IsAny<Uri>()), Moq.Times.Once);
+
+            Assert.Fail("unwritten");
+            //WorkoutDataProvider
+        }
+    }
+}
