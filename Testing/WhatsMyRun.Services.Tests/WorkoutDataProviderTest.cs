e438bdb733049e40f2ef690610c20a884a845b40
Ryan Betker <ryan_betker@hotmail.com>
2014-05-25 09:46:56 -0500

    Test to be proper async. Dave to make the test pass

--- Testing/WhatsMyRun.Services.Tests/WorkoutDataProviderTest.cs
+++ Testing/WhatsMyRun.Services.Tests/WorkoutDataProviderTest.cs
@@ -14,22 +14,21 @@ namespace WhatsMyRun.Tests
     {
         private string dataLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\SampleWorkoutsData.json";
         [TestMethod]
-        public void WorkoutDataProvider_Default_CallsRequestor()
+        public async Task WorkoutDataProvider_Default_CallsRequestor()
         {
             //ARRANGE
             var mockRequestor = new Moq.Mock<IDataRequestor>();
             var mockedData = File.ReadAllText(dataLocation);
             var provider = new WorkoutDataProvider(mockRequestor.Object);
-            mockRequestor.Setup(r => r.GetDataAsync<string>(It.IsAny<Uri>()))
+            mockRequestor.Setup(r => r.GetDataAsync(It.IsAny<Uri>()))
                 .Returns(Task.FromResult(mockedData));
 
             //To see the test fail, bring back this line:
-            //mockRequestor.Setup(r => r.GetDataAsync<int>(It.IsAny<Uri>()))
-            //    .Returns(Task.FromResult(1));
+            mockRequestor.Setup(r => r.GetDataAsync(new Uri("http://test.com")))
+                .Returns(Task.FromResult(mockedData));
 
             //ACT
-            provider.GetWorkoutsForUserAsync(1)
-                .Wait();
+            await provider.GetWorkoutsForUserAsync(1);
             mockRequestor.VerifyAll();
         }
     }
