64c2666b2a3c3cef29f0a01a13c38558d427e193
Ryan Betker <ryan_betker@hotmail.com>
2014-06-02 00:31:32 -0500

    Adding JTokenExtension for Value with default if node not found.

---
+++ WhatsMyRun.Services/Common/JTokenExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newtonsoft.Json.Linq
+{
+    public static class JTokenExtensions
+    {
+        /// <summary>
+        /// Provides a fallback value if the node was not found
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="token"></param>
+        /// <param name="valueToUseIfNotFound"></param>
+        /// <returns></returns>
+        public static T ValueWithDefault<T>(this JToken token, T valueToUseIfNotFound = default(T))
+        {
+            if (token != null)
+            {
+                return token.Value<T>();
+            }
+            else
+            {
+                return valueToUseIfNotFound;
+            }
+        }
+    }
+}
--- WhatsMyRun.Services/DataProviders/Workouts/WorkoutDataProvider.cs
+++ WhatsMyRun.Services/DataProviders/Workouts/WorkoutDataProvider.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using WhatsMyRun.DataModel.Workouts;
 using WhatsMyRun.Services.DataRequestor;
+using System.Linq;
 
 namespace WhatsMyRun.Services.DataProviders.Workouts
 {
@@ -26,14 +27,30 @@ namespace WhatsMyRun.Services.DataProviders.Workouts
             var workoutData = JObject.Parse(workoutDataString);
 
             var workouts = new List<WorkoutDataModel>();
-            
-            foreach (JObject workoutObj in workoutData["_embedded"]["workouts"]) //workoutData.GetValue("_embedded$workouts"))
+
+            var wCount = workoutData["_embedded"]["workouts"].Children().Count();
+            foreach (JObject workoutObj in workoutData["_embedded"]["workouts"])
             {
-                var workout = new WorkoutDataModel();
-                workout.ActiveTimeInSeconds = workoutObj["aggregates"]["active_time_total"].Value<double>();
+                var workout = ParseWorkout(workoutObj);
                 workouts.Add(workout);
             }
             return workouts;
+        }
+
+        private static WorkoutDataModel ParseWorkout(JObject workoutObj)
+        {
+            var workout = new WorkoutDataModel();
+
+            workout.ActiveTimeInSeconds = workoutObj["aggregates"]["active_time_total"].ValueWithDefault<double>();
+            workout.AverageSpeedInX = workoutObj["aggregates"]["speed_avg"].ValueWithDefault<double>();
+            workout.DistanceInMeters = workoutObj["aggregates"]["distance_total"].ValueWithDefault<double>();
+            workout.ElapsedTimeInSeconds = TimeSpan.FromSeconds(workoutObj["aggregates"]["elapsed_time_total"].ValueWithDefault<double>());
+            workout.MetabolicEnergeyTotal = workoutObj["aggregates"]["metabolic_energy_total"].ValueWithDefault<double>();
+            workout.Notes = workoutObj["notes"].ValueWithDefault<string>();
+            workout.StartTime = workoutObj["start_datetime"].ValueWithDefault<DateTime>();
+            workout.TotalSteps = workoutObj["aggregates"]["steps_total"].ValueWithDefault<int>();
+
+            return workout;
         }
     }
 }
